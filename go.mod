332da28fd4c334df16868bfe4c450236c1fe2378
apodl <a.podlozny@gmail.com>
2025-12-28 14:27:38 +0200

    feat: change password hashing to use newer and recommended argon2id

--- go.mod
+++ go.mod
@@ -1,14 +1,15 @@
 module advancedauth
 
-go 1.23.0
+go 1.24.0
 
 require (
 	github.com/maddalax/htmgo/framework v1.0.7-0.20250703190716-06f01b3d7c1b
 	github.com/mattn/go-sqlite3 v1.14.24
-	golang.org/x/crypto v0.28.0
+	golang.org/x/crypto v0.46.0
 )
 
 require (
 	github.com/go-chi/chi/v5 v5.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
 )
--- internal/user/password.go
+++ internal/user/password.go
@@ -1,18 +1,81 @@
 package user
 
 import (
-	"golang.org/x/crypto/bcrypt"
+  "crypto/rand"
+  "crypto/subtle"
+	"encoding/base64"
+	"fmt"
+	"strings"
+
+	"golang.org/x/crypto/argon2"
 )
 
+type argonParams struct {
+	memory      uint32
+	iterations  uint32
+	parallelism uint8
+	saltLength  uint32
+	keyLength   uint32
+}
+
 func HashPassword(password string) (string, error) {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		return "", err
-	}
-	return string(hashedPassword), nil
+	// Recommended parameters for Argon2id (RFC 9106, retrieved 2025)
+	p := &argonParams{
+			memory:      64 * 1024, // 64MB
+			iterations:  3,
+			parallelism: 2, // Default value, adjust per core count if needed
+			saltLength:  16,
+			keyLength:   32,
+		}
+
+	salt := make([]byte, p.saltLength)
+		if _, err := rand.Read(salt); err != nil {
+			return "", err
+		}
+
+	hash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
+
+	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+
+	encodedPassword := fmt.Sprintf(
+		"$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s",
+		argon2.Version,
+		p.memory,
+		p.iterations,
+		p.parallelism,
+		b64Salt,
+		b64Hash,
+	)
+
+	return string(encodedPassword), nil
 }
 
 func PasswordMatches(password string, hashedPassword string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
-	return err == nil
+		parts := strings.Split(hashedPassword, "$")
+		if len(parts) != 6 {
+			return false
+		}
+
+		var p argonParams
+		_, err := fmt.Sscanf(parts[3], "m=%d,t=%d,p=%d", &p.memory, &p.iterations, &p.parallelism)
+		if err != nil {
+			return false
+		}
+
+		salt, err := base64.RawStdEncoding.DecodeString(parts[4])
+		if err != nil {
+			return false
+		}
+
+		decodedHash, err := base64.RawStdEncoding.DecodeString(parts[5])
+		if err != nil {
+			return false
+		}
+
+		// Re-hash the input password with the same parameters
+		comparisonHash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, uint32(len(decodedHash)))
+
+		// Use ConstantTimeCompare to prevent timing attacks
+		return subtle.ConstantTimeCompare(decodedHash, comparisonHash) == 1
 }
